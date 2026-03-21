c511fc6c608c442c4311addce227d6bee8e5abcf
Dipak Yadav <dipakyadav12221@gmail.com>
2026-03-21 13:00:01 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+   <>
+    <div className="pap">
+      <h1>Vite + React</h1>
+    </div>
+    <div className="chacha">
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+   </>
+  )
+}
+export default App
