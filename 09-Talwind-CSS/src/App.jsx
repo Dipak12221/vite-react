df8057b7824c1761190a1f748d3f4710b5772d61
Dipak Yadav <dipakyadav12221@gmail.com>
2026-04-19 18:53:53 +0530

    Update Talwind css

--- 09-Talwind-CSS/src/App.jsx
+++ 09-Talwind-CSS/src/App.jsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold text-center text-blue-500">
+    <div className="text-3xl font-bold text-center text-blue-500 rounded-xl bg-blue-200 p-4 m-4">
       Hello is my first Tailwind CSS project
     </div>
   )
