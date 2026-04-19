1c4242e508ae4d3b547e202fc77a7abd39553c80
Dipak Yadav <dipakyadav12221@gmail.com>
2026-04-19 18:47:02 +0530

    Talwind CSS

---
+++ 09-Talwind-CSS/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold text-center text-blue-500">
+      Hello is my first Tailwind CSS project
+    </div>
+  )
+}
+
+export default App
